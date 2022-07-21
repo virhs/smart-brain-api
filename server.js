714717a0dd7d2ec962ed1469bb8bfc51cb192f86
Virmani <virmanihs@gmail.com>
2022-07-21 17:02:22 +0530

    some changes

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/',(req,res)=>{
-    res.json(db)
+    res.json('its working')
 })
 
 app.post('/signin',(req,res)=>{onSignin(req,res,bcrypt,db)})
