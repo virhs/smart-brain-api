cfdb5b8a7a2b7ad07fcf5be5e36e7cef1abed8e4
Virmani <virmanihs@gmail.com>
2022-07-21 23:58:47 +0530

    again db changes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const onImage = require('./controllers/entry-count');
 const db = knex({
     client: 'pg',
     connection: {
-      host : process.env.DATABASE_URL,
+      connectionString: process.env.DATABASE_URL,
       ssl: {
         rejectUnauthorized: false
       }
