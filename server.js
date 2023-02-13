4d920f59950208885c9206cffb25db4bdde3427d
virhs <68024365+virhs@users.noreply.github.com>
2023-02-13 14:32:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const db = knex({
     client: 'pg',
     connection: {
       host : process.env.PGHOST,
-      port : process.env.PORT,
+      port : process.env.PGPORT,
       user : process.env.PGUSER,
       password : process.env.PGPASSWORD,
       database : process.env.PGDATABASE
